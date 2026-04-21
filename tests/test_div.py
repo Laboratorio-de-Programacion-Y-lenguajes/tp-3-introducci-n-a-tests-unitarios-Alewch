dec937aebb1e4a109b12cb27ada909607c670f1c
alewch <leonelchambi210@gmail.com>
2026-04-21 19:28:22 -0300

    agrego tests de la funcion div

--- tests/test_div.py
+++ tests/test_div.py
@@ -22,3 +22,12 @@ def test_div_normal():
 # def test_div_por_cero():
 #     with pytest.raises(ZeroDivisionError):
 #         div(10, 0)
+def test_resul_decimal():
+    assert div(7.0, 2.0) == 3.5
+
+def test_num_negativo():
+    assert div (-5.0,-5.0) == 1
+
+def test_division_cero():
+    with pytest.raises(ZeroDivisionError) :
+        assert div(5.0, 0)
